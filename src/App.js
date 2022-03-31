eb471eecc8e4f8eb43ed5f01a430f1d17962c98e
Isaque Lima Leite <Isaque.lima.leite.ill@gmail.com>
2022-03-31 06:49:17 +0000

    feat: import Background component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React from "react";
+import { Background } from "./components/Background";
+import { Logo } from "./components/Background/logo";
+
+
+
 function App() {
     return ( 
     <div className = "App" > 
+        <Background></Background>
     </div>
     
     );
