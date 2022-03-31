2fcfe7d1d8185569c3b0afcebc00c8b04fc3411a
Isaque Lima Leite <Isaque.lima.leite.ill@gmail.com>
2022-03-31 06:49:39 +0000

    feat: add styled component

---
+++ src/assets/css/styles.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Back = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #079441;
+`;
